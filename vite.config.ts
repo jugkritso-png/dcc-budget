f80c94f27e020f3be994a466710c4e8db6a2cfc4
jakrit.so <jakrit.so@mail.wu.ac.th>
2026-02-19 10:55:23 +0700

    Fix build: remove manualChunks configuration entirely

--- vite.config.ts
+++ vite.config.ts
@@ -32,14 +32,6 @@ export default defineConfig(({ mode }) => {
     },
     build: {
       chunkSizeWarningLimit: 1000, // Increase limit to 1000kB
-      rollupOptions: {
-        output: {
-          manualChunks: {
-            vendor: ['react', 'react-dom'],
-            utils: ['lodash'] // removed axios as it's not a dependency
-          }
-        }
-      }
     }
   };
 });
